01da5f2d4bd327b7f34a6a1a978df03c23bb6732
Shivam upadhyay <shivam.upadhyay@codinova.com>
2021-11-30 10:39:48 +0530

    add v2
    Signed-off-by: Shivam upadhyay <shivam.upadhyay@codinova.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <amplify-authenticator username-alias="phone_number">
+    <amplify-sign-up
+      slot="sign-up"
+      username-alias="phone_number"
+      :form-fields.prop="signUpformFields"
+    ></amplify-sign-up>
+    <amplify-sign-in slot="sign-in" username-alias="phone_number">
+      <div slot="federated-buttons"></div>
+    </amplify-sign-in>
+    <amplify-sign-out></amplify-sign-out>
+    <chat-window
+      :current-user-id="currentUserId"
+      :rooms="rooms"
+      :messages="messages"
+    />
+  </amplify-authenticator>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ChatWindow from "vue-advanced-chat";
+import "vue-advanced-chat/dist/vue-advanced-chat.css";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ChatWindow,
+  },
+  data() {
+    return {
+      rooms: [
+        {
+          roomId: 1,
+          roomName: "Room 1",
+          avatar: "assets/imgs/people.png",
+          unreadCount: 1,
+          index: 3,
+          lastMessage: {
+            content: "Last message received",
+            senderId: 1234,
+            username: "John Doe",
+            timestamp: "10:20",
+            saved: true,
+            distributed: false,
+            seen: true,
+            new: false,
+          },
+          users: [
+            {
+              _id: 1234,
+              username: "John Doe",
+              avatar: "assets/imgs/doe.png",
+              status: {
+                state: "online",
+                lastChanged: "today, 14:30",
+              },
+            },
+          ],
+        },
+      ],
+      messages: [],
+      currentUserId: 1234,
+      signUpformFields: [
+        {
+          type: "name",
+          placeholder: "Enter your name",
+          required: true,
+          label: "Name",
+        },
+        {
+          type: "phone_number",
+        },
+        {
+          type: "password",
+          required: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
