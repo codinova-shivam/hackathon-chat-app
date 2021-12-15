da6a25fdf049b16995d6e82eeee5584aee472e20
Shivam upadhyay <shivam.upadhyay@codinova.com>
2021-12-15 11:06:54 +0530

    remove dial code
    Signed-off-by: Shivam upadhyay <shivam.upadhyay@codinova.com>

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,6 @@ import { onAuthUIStateChange } from "@aws-amplify/ui-components";
 const resetPasswordFormFields = [
   {
     type: "phone_number",
-    dialCode: "+91",
     required:true
   },
 ];
